a30367bd2ec6580ce8439e8935e3183b787df3b2
Stefan Loehers <stef@loehers.de>
2012-05-17 19:43:14 +0200

    init a phonegap android app, can take photo

---
+++ src/de/upb/fsln12/pecs/PecsActivity.java
@@ -0,0 +1,13 @@
+package de.upb.fsln12.pecs;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class PecsActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
