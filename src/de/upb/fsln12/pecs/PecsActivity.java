7fbd4e09fe30bacbd5fb0b7b40bacaae5d7d680e
Stefan Loehers <stef@loehers.de>
2012-06-05 23:42:40 +0200

    Change index.html to remote site http://pecs.herokuapp.com/

--- src/de/upb/fsln12/pecs/PecsActivity.java
+++ src/de/upb/fsln12/pecs/PecsActivity.java
@@ -8,6 +8,7 @@ public class PecsActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        //super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("http://pecs.herokuapp.com/");
     }
 }
